d01cc4192751d1489816722e8b14ce553fadcf70
Ben <ben216033@gmail.com>
2022-05-24 16:20:01 +0800

    通訊錄新增欄位

--- proj/ab_add.php
+++ proj/ab_add.php
@@ -12,20 +12,43 @@ $title = '新增通訊錄資料-First_Web';
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">新增資料</h5>
-                    <form>
+                    <form name="form1" onsubmit="return false">
+
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
+                            <!-- required是必填欄位 -->
+                            <div class="form-text"></div>
                         </div>
+
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="email" name="email">
+                            <div class="form-text"></div>
                         </div>
-                        <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+                        <div class="mb-3">
+                            <label for="mobile" class="form-label">mobile</label>
+                            <!-- 這邊type也是text -->
+                            <input type="text" class="form-control" id="mobile" name="mobile" pattern="09\d{8}">
+                            <!-- pattern是用來限制格式的 -->
+                            <div class="form-text"></div>
                         </div>
+
+                        <div class="mb-3">
+                            <label for="birthday" class="form-label">Email address</label>
+                            <input type="date" class="form-control" id="birthday" name="birthday">
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="address" class="form-label">Address</label>
+                            <textarea class="form-control" id="address" name="address" cols="30" rows="10"></textarea>
+                            <div class="form-text"></div>
+                        </div>
+
+
+
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
