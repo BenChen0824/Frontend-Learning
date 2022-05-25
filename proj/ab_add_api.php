6edda7c4c83db89551f07fd022ee7ec02baad041
Ben <ben216033@gmail.com>
2022-05-25 10:07:04 +0800

    isset vs empty & lastInsertId

--- proj/ab_add_api.php
+++ proj/ab_add_api.php
@@ -43,8 +43,17 @@ $stmt->execute([
 if ($stmt->rowCount() == 1) {
     //新增的筆數是否為1 是的話為true
     $output['success'] = true;
+
+    // 最近新增資料的 primery key
+    $output['lastInsertId'] = $pdo->lastInsertId();
 } else {
     $output['error'] = "資料無法新增";
 }
+
+
+//isset vs empty 
+//isset就算設定為undefined或是NULL都會算是有設定過
+//empty則是沒設定或是為空值都匯回傳true
+
 
 echo json_encode($output, JSON_UNESCAPED_UNICODE);
