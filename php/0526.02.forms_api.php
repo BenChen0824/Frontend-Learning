fdd199df798fc9691b51d4cd50bfbe1e3f0f429a
Ben <ben216033@gmail.com>
2022-05-26 11:55:28 +0800

    按鈕輸出時JSON 字串存成檔案

--- php/0526.02.forms_api.php
+++ php/0526.02.forms_api.php
@@ -4,4 +4,8 @@ $output = [
     'postData' => $_POST,
 ];
 
-echo json_encode($output, JSON_UNESCAPED_UNICODE);
+$json = json_encode($output, JSON_UNESCAPED_UNICODE);
+file_put_contents('./0526.02.forms_api.json', $json);  // JSON 字串存成檔案
+
+
+echo $json;
