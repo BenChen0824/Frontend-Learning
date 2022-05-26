bf76d0c54a3dc35834e9ceadbf4cf1b53c986eb6
Ben <ben216033@gmail.com>
2022-05-26 12:26:49 +0800

    利用另一張表把json資料抓出來

--- php/0526.02.forms_api.php
+++ php/0526.02.forms_api.php
@@ -1,11 +1,12 @@
 <?php
 
 $output = [
-    'postData' => $_POST,
+    'postdata' => $_POST,
 ];
 
 $json = json_encode($output, JSON_UNESCAPED_UNICODE);
 file_put_contents('./0526.02.forms_api.json', $json);  // JSON 字串存成檔案
+//重複輸入會蓋掉之前檔案
 
 
 echo $json;
